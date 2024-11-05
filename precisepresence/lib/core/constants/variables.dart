0dd230348dcb41d5159a3a443d755fe7ad49d47b
Shofwah Kanaka <shofwahknk@gmail.com>
2024-11-05 11:43:52 +0700

    hapus flutter build dari awal

--- precisepresence/lib/core/constants/variables.dart
+++
@@ -1,7 +0,0 @@
-class Variables {
-  static const String baseUrl = 'http://192.168.18.31:8000';
-
-  static const String baseUrlImage = '$baseUrl/storage//';
-
-  static const String rajaOngkirKey = 'a9da3c4359fafde97f03ee2be60147b2';
-}
