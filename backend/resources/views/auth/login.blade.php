c9105d31cd4930ad1fc64d173c22c6adf23f8ada
kodok <77190656+uzidreamcode@users.noreply.github.com>
2024-11-29 01:34:32 +0700

    feat: redesign login page with new layout and improved user interface

--- backend/resources/views/auth/login.blade.php
+++ backend/resources/views/auth/login.blade.php
@@ -1,47 +1,146 @@
-<x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+<!DOCTYPE html>
+<html lang="en">
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
+<head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+    <title>Login &mdash; Stisla</title>
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+    <!-- General CSS Files -->
+    <link rel="stylesheet" href="./../../library/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+        integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+    <!-- CSS Libraries -->
+    <link rel="stylesheet" href="./../../library/bootstrap-social/bootstrap-social.css">
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
+    <!-- Template CSS -->
+    <link rel="stylesheet" href="./../../css/style.css">
+    <link rel="stylesheet" href="./../../css/components.css">
+    <!-- Start GA -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-94034622-3"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
 
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
-        </div>
+        gtag('config', 'UA-94034622-3');
+    </script>
+    <!-- END GA -->
+</head>
 
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
+<body>
+    <div id="app">
+        <section class="section">
+            <div class="container mt-5">
+                <div class="row">
+                    <div
+                        class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
+                        <!-- Footer -->
+                        <div class="login-brand">
+                            <img src="http://laravel-stisla.test/img/stisla-fill.svg" alt="logo" width="100"
+                                class="shadow-light rounded-circle">
+                        </div>
 
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+                        <!-- Content -->
+                        <div class="card card-primary">
+                            <div class="card-header">
+                                <h4>Login</h4>
+                            </div>
+
+                            <div class="card-body">
+                                @if (session('status'))
+                                <div class="mb-4 text-sm text-green-600">
+                                    {{ session('status') }}
+                                </div>
+                            @endif
+                                <form method="POST"
+                                    action="{{ route('login') }}"
+                                    class="needs-validation"
+                                    novalidate="">
+                                    <div class="form-group">
+                                        <label for="email">Email</label>
+                                        <input id="email"
+                                            type="email"
+                                            class="form-control"
+                                            name="email"
+                                            tabindex="1"
+                                            required
+                                            autofocus>
+                                            @error('email')
+                                            <span class="text-red-500 text-sm mt-2">{{ $message }}</span>
+                                        @enderror
+                                        <div class="invalid-feedback">
+                                            Please fill in your email
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <div class="d-block">
+                                            <label for="password"
+                                                class="control-label">Password</label>
+
+                                        </div>
+                                        <input id="password"
+                                            type="password"
+                                            class="form-control"
+                                            name="password"
+                                            tabindex="2"
+                                            required>
+                                            @error('password')
+                                            <span class="text-red-500 text-sm mt-2">{{ $message }}</span>
+                                        @enderror
+                                        <div class="invalid-feedback">
+                                            please fill in your password
+                                        </div>
+                                    </div>
+
+
+
+                                    <div class="form-group">
+                                        <button type="submit"
+                                            class="btn btn-primary btn-lg btn-block"
+                                            tabindex="4">
+                                            Login
+                                        </button>
+                                    </div>
+                                </form>
+
+
+                            </div>
+                        </div>
+
+
+                        <!-- Footer -->
+                        <div class="simple-footer">
+                            Copyright &copy; Stisla 2018
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+
+    <!-- General JS Scripts -->
+    <script src="./../../library/jquery/dist/jquery.min.js"></script>
+    <script src="./../../library/popper.js/dist/umd/popper.js"></script>
+    <script src="./../../library/tooltip.js/dist/umd/tooltip.js"></script>
+    <script src="./../../library/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="./../../library/jquery.nicescroll/dist/jquery.nicescroll.min.js"></script>
+    <script src="./../../library/moment/min/moment.min.js"></script>
+    <script src="./../../js/stisla.js"></script>
+
+    <!-- JS Libraies -->
+
+    <!-- Page Specific JS File -->
+
+    <!-- Template JS File -->
+    <script src="./../../js/scripts.js"></script>
+    <script src="./../../js/custom.js"></script>
+</body>
+
+</html>
