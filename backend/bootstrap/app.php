41cc2a635f8fa2b1a27c08be55a9c0776da8c9f5
kodok <77190656+uzidreamcode@users.noreply.github.com>
2024-12-05 20:31:17 +0700

    buat ssh

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -17,6 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'check-out',
             'perizinan',
             'login',
+            'absensi',
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
