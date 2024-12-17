8776edb42ac83ec58988c276534fafe93260eda9
azahrasalsaa <azahras751@gmail.com>
2024-12-17 20:17:08 +0700

    settings

--- backend/resources/views/hrd/pengaturan/cabang.blade.php
+++ backend/resources/views/hrd/pengaturan/cabang.blade.php
@@ -3,63 +3,66 @@
 @section('title', 'Settings')
 
 @section('main')
-    <div class="main-content">
-        <section class="section">
-            <div class="section-header">
-                <h1>Settings</h1>
-                <div class="section-header-button">
-                    <a href="{{ route('hrd.pengaturan.cabang.tambah') }}" class="btn btn-primary">Tambah Cabang</a>
-                </div>
-
-                <div class="section-header-breadcrumb">
-                    <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
-                    <div class="breadcrumb-item">Settings</div>
-
-                </div>
+<div class="main-content">
+    <section class="section">
+        <div class="section-header">
+            <div class="section-header-back">
+                <a href="javascript:void(0);" onclick="history.back();" class="btn btn-icon"><i class="fas fa-arrow-left"></i></a>
+            </div>
+            <h1>Settings</h1>
+            <div class="section-header-button">
+                <a href="{{ route('hrd.pengaturan.cabang.tambah') }}" class="btn btn-primary">Tambah Cabang</a>
             </div>
 
-            <div class="section-body">
-                <div class="row">
+            <div class="section-header-breadcrumb">
+                <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
+                <div class="breadcrumb-item">Settings</div>
 
-                    @foreach ($cabang as $c)
-                    <div class="col-12 col-sm-6 col-md-6 col-lg-3">
-                        <article class="article">
-                            <div class="article-header">
-                                <div class="article-image"
+            </div>
+        </div>
+
+        <div class="section-body">
+            <div class="row">
+
+                @foreach ($cabang as $c)
+                <div class="col-12 col-sm-6 col-md-6 col-lg-3">
+                    <article class="article">
+                        <div class="article-header">
+                            <div class="article-image"
                                 style="background-image: url('{{ asset($c->foto_cabang) }}');">
-                           </div>
+                            </div>
 
-                                <div class="article-title">
-                                    <h2><a href="#">
+                            <div class="article-title">
+                                <h2><a href="#">
                                         {{ $c->nama_cabang }}
                                     </a></h2>
-                                </div>
                             </div>
-                            <div class="article-details">
-                                <p>
-                                    {{ $c->alamat_cabang }}
-                                </p>
-                                <div class="article-cta">
-                                    <a href="{{ route('hrd.pengaturan.cabang.detail', $c->id_cabang) }}"
-                                        class="btn btn-primary">Read More</a>
-                                </div>
+                        </div>
+                        <div class="article-details">
+                            <p>
+                                {{ $c->alamat_cabang }}
+                            </p>
+                            <div class="article-cta">
+                                <a href="{{ route('hrd.pengaturan.cabang.detail', $c->id_cabang) }}"
+                                    class="btn btn-primary">Read More</a>
                             </div>
-                        </article>
-                    </div>
-                    @endforeach
-
+                        </div>
+                    </article>
                 </div>
+                @endforeach
 
             </div>
-        </section>
-    </div>
+
+        </div>
+    </section>
+</div>
 
 
 
 @endsection
 
 @push('scripts')
-    <!-- JS Libraies -->
+<!-- JS Libraies -->
 
-    <!-- Page Specific JS File -->
+<!-- Page Specific JS File -->
 @endpush
--- backend/resources/views/hrd/pengaturan/index.blade.php
+++ backend/resources/views/hrd/pengaturan/index.blade.php
@@ -3,62 +3,65 @@
 @section('title', 'Settings')
 
 @push('style')
-    <!-- CSS Libraries -->
+<!-- CSS Libraries -->
 @endpush
 
 @section('main')
-    <div class="main-content">
-        <section class="section">
-            <div class="section-header">
-                <h1>Settings</h1>
-                <div class="section-header-breadcrumb">
-                    <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
-                    <div class="breadcrumb-item">Settings</div>
-                </div>
+<div class="main-content">
+    <section class="section">
+        <div class="section-header">
+            <div class="section-header-back">
+                <a href="javascript:void(0);" onclick="history.back();" class="btn btn-icon"><i class="fas fa-arrow-left"></i></a>
             </div>
+            <h1>Settings</h1>
+            <div class="section-header-breadcrumb">
+                <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
+                <div class="breadcrumb-item">Settings</div>
+            </div>
+        </div>
 
-            <div class="section-body">
-                <h2 class="section-title">Overview</h2>
-                <p class="section-lead">
-                    Organize and adjust all settings about this site.
-                </p>
+        <div class="section-body">
+            <!-- <h2 class="section-title">Overview</h2>
+            <p class="section-lead">
+                Organize and adjust all settings about this site.
+            </p> -->
 
-                <div class="row">
-                    <div class="col-lg-6">
-                        <div class="card card-large-icons">
-                            <div class="card-icon bg-primary text-white">
-                                <i class="fas fa-cog"></i>
-                            </div>
-                            <div class="card-body">
-                                <h4>Devisi</h4>
-                                <p>General settings such as, site title, site description, address and so on.</p>
-                                <a href="{{ route('hrd.pengaturan.general') }}"
-                                    class="card-cta">Change Setting <i class="fas fa-chevron-right"></i></a>
-                            </div>
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="card card-large-icons">
+                        <div class="card-icon bg-primary text-white">
+                            <i class="fas fa-cog"></i>
+                        </div>
+                        <div class="card-body">
+                            <h4>Devisi</h4>
+                            <!-- <p>General settings such as, site title, site description, address and so on.</p> -->
+                            <a href="{{ route('hrd.pengaturan.general') }}"
+                                class="card-cta">Change Setting <i class="fas fa-chevron-right"></i></a>
                         </div>
                     </div>
-                    <div class="col-lg-6">
-                        <div class="card card-large-icons">
-                            <div class="card-icon bg-primary text-white">
-                                <i class="fas fa-search"></i>
-                            </div>
-                            <div class="card-body">
-                                <h4>Cabang</h4>
-                                <p>Search engine optimization settings, such as meta tags and social media.</p>
-                                <a href="{{ route('hrd.pengaturan.cabang') }}"
-                                    class="card-cta">Change Setting <i class="fas fa-chevron-right"></i></a>
-                            </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="card card-large-icons">
+                        <div class="card-icon bg-primary text-white">
+                            <i class="fas fa-search"></i>
+                        </div>
+                        <div class="card-body">
+                            <h4>Cabang</h4>
+                            <!-- <p>Search engine optimization settings, such as meta tags and social media.</p> -->
+                            <a href="{{ route('hrd.pengaturan.cabang') }}"
+                                class="card-cta">Change Setting <i class="fas fa-chevron-right"></i></a>
                         </div>
                     </div>
-
                 </div>
+
             </div>
-        </section>
-    </div>
+        </div>
+    </section>
+</div>
 @endsection
 
 @push('scripts')
-    <!-- JS Libraies -->
+<!-- JS Libraies -->
 
-    <!-- Page Specific JS File -->
+<!-- Page Specific JS File -->
 @endpush
